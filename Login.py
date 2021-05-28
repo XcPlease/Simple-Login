f0dc6df4fc4641a60f279d0c732f4ca8a5edce44
XcPlease <84813600+XcPlease@users.noreply.github.com>
2021-05-28 13:15:42 +0000

    Update Login.py

--- Login.py
+++ Login.py
@@ -1,10 +1,10 @@
 import requests
-info = requests.get("https://pastebin.com/raw/NGsTFapN")
-username = input("Enter your username: ")
-password = input("Enter your pass: ")
-if username and password in info.text:
-    print("Do What Ever You Want")
+info = requests.get("https://pastebin.com/raw/yoururl") # Accessing The Pasebin link...
+username = input("Enter your username: ") # Username Input
+password = input("Enter your pass: ") # Password Input
+if username and password in info.text: # Checks if the user inputs are in the pastebin 
+    print("Do What Ever You Want") # If the user inputs are correct do 'code code code'
 # code code code
-else:
+else: # if the user credentials are wrong do 'code code code'
     print("Invalid Login Info")
  
